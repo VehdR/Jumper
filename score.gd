18086ad724b879c772576763b0150cb31a390caa
VehdR <87544515+VehdR@users.noreply.github.com>
2023-05-24 10:33:10 -0700

    Got score counter in quest working

---
+++ score.gd
@@ -0,0 +1,17 @@
+extends Control
+
+var score = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+
+func _on_ui_gem_score():
+	score = score + 42
+	$Label.text = str(score)
--- ui.gd
+++ ui.gd
@@ -2,6 +2,7 @@ extends Control
 
 var gems_collected = 0
 signal all_gems_collected
+signal gem_score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,6 +18,8 @@ func _on_gem_gem_collected():
 	
 	if gems_collected == 10:
 		emit_signal("all_gems_collected")
+	
+	emit_signal("gem_score")
 
 func _on_character_death():
 	gems_collected = 0
