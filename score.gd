b93653cac322db4f52ea43b53884c29146f876b9
VehdR <87544515+VehdR@users.noreply.github.com>
2023-06-05 21:57:49 -0700

    Fixed score updating across quest levels

--- score.gd
+++ score.gd
@@ -1,23 +1,23 @@
 extends Control
 
-var score = 0
+var score = Global.score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	score = Global.score
+	$Label.text = str(score)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
 
 func _on_ui_gem_score():
 	score = score + 42
 	$Label.text = str(score)
-	
 	Global.score = score
 
 func _on_skeleton_skeleton_score():
 	score = score + 100
 	$Label.text = str(score)
+	Global.score = score
