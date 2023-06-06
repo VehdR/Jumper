b9526adb0fc9b5593de51f244ade26599a5b9beb
VehdR <87544515+VehdR@users.noreply.github.com>
2023-06-05 17:40:33 -0700

    reverting to older version

--- character.gd
+++ character.gd
@@ -3,15 +3,13 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -430.0
-var start_pos
+var start_pos = Vector2(411, 63)
 signal death
 var num_jumps = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _ready():
-	start_pos = position
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("exit_to_menu"):
@@ -53,14 +51,14 @@ func _physics_process(delta):
 
 	move_and_slide()
 	
-func die():
+
+func _on_die_box_player_entered():
 	emit_signal("death")
 	self.position = start_pos
 	velocity = Vector2(0, 0)
 	$AnimatedSprite2D.play("idle")
+	
 
-func _on_die_box_player_entered():
-	die()
 
 func _on_teleporter_body_entered(body):
 	pass # Replace with function body.
--- skeleton.gd
+++ skeleton.gd
@@ -13,7 +13,10 @@ var curstate = State.WALK_RIGHT
 var state_time = 0.0
 var HEALTH = 1
 var hits = 0
+<<<<<<< HEAD
 signal player_attack
+=======
+>>>>>>> parent of 256d626 (Added player death to the skeleton)
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -51,12 +54,18 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _on_area_2d_area_entered(area):
+<<<<<<< HEAD
 	print("LOL")
+=======
+>>>>>>> parent of 256d626 (Added player death to the skeleton)
 	if curstate == State.WALK_RIGHT:
 		switch_to(State.WALK_LEFT)
 	else:
 		switch_to(State.WALK_RIGHT)
+<<<<<<< HEAD
 		
 	if area.is_in_group("Character"):
 		print("joe")
 		emit_signal("player_attack")
+=======
+>>>>>>> parent of 256d626 (Added player death to the skeleton)
