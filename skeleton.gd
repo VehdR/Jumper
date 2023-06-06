629d2c608e450f9c1b0d78d5195703a6e099e982
VehdR <87544515+VehdR@users.noreply.github.com>
2023-06-05 17:50:39 -0700

    cleaned up character code

--- skeleton.gd
+++ skeleton.gd
@@ -13,10 +13,7 @@ var curstate = State.WALK_RIGHT
 var state_time = 0.0
 var HEALTH = 1
 var hits = 0
-<<<<<<< HEAD
 signal player_attack
-=======
->>>>>>> parent of 256d626 (Added player death to the skeleton)
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -54,18 +51,12 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _on_area_2d_area_entered(area):
-<<<<<<< HEAD
 	print("LOL")
-=======
->>>>>>> parent of 256d626 (Added player death to the skeleton)
 	if curstate == State.WALK_RIGHT:
 		switch_to(State.WALK_LEFT)
 	else:
 		switch_to(State.WALK_RIGHT)
-<<<<<<< HEAD
 		
 	if area.is_in_group("Character"):
 		print("joe")
 		emit_signal("player_attack")
-=======
->>>>>>> parent of 256d626 (Added player death to the skeleton)
